3ec87c6ec003c51d6a41fa6bb6a960d26ba13254
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2023-10-30 21:35:58 +0000

    fixed animation when scrolling onto home page fixed some minor bugs

--- src/pages/home/script.js
+++ src/pages/home/script.js
@@ -1,69 +1,82 @@
 function toggleMenu() {
-  var menu = document.getElementById("menu");
-  var menuToggle = document.getElementById("menuToggle");
+    var menu = document.getElementById("menu");
+    var menuToggle = document.getElementById("menuToggle");
 
-  menu.classList.toggle("active");
-  menuToggle.classList.toggle("active");
+    menu.classList.toggle("active");
+    menuToggle.classList.toggle("active");
 
-  if (window.innerWidth > 512) {
-    menu.classList.remove("active");
-    menuToggle.classList.remove("active");
-  }
-}
-
-// Função para verificar se uma seção está visível no viewport
-function isElementInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
-  );
-}
-
-// Função para ativar animações de deslize quando as seções estão no viewport
-function animateOnScroll() {
-  const sections = document.querySelectorAll(".container-sections section");
-
-  sections.forEach((section) => {
-    const isVisible = isElementInViewport(section);
-
-    if (isVisible) {
-      section.classList.add("active");
-    } else {
-      section.classList.remove("active");
+    if (window.innerWidth > 512) {
+        menu.classList.remove("active");
+        menuToggle.classList.remove("active");
     }
-  });
-}
-
-// Adicione um ouvinte de eventos de rolagem para ativar as animações
-window.addEventListener("scroll", animateOnScroll);
-window.addEventListener("load", animateOnScroll); // Ative as animações quando a página é carregada
-
-// Função para rolar suavemente para uma seção
-function smoothScroll(target) {
-  const element = document.querySelector(target);
-  if (element) {
-    window.scrollTo({
-      behavior: "smooth",
-      top: element.offsetTop,
-    });
-  }
-}
-
-// Ouvintes de eventos para links do menu
-document.querySelectorAll(".menu button").forEach((button) => {
-  button.addEventListener("click", (e) => {
-    e.preventDefault();
-    smoothScroll(button.getAttribute("onclick").split("'")[1]);
-  });
-});
+}   
 
 window.addEventListener("resize", function () {
-  if (window.innerWidth > 512) {
+    if (window.innerWidth > 512) {
     var menu = document.getElementById("menu");
     var menuToggle = document.getElementById("menuToggle");
 
     menu.classList.remove("active");
     menuToggle.classList.remove("active");
-  }
+    }
 });
+
+function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+    rect.top >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+}
+  
+function animateOnScroll() {
+    const sections = document.querySelectorAll(".container-section-text");
+  
+    sections.forEach((section) => {
+      const rect = section.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+      const visibilityHeight = windowHeight - 50;
+  
+      let opacity = 1;
+      if (rect.top > visibilityHeight) {
+        opacity = 0;
+      } else if (rect.top < 0) {
+        opacity = 1;
+      } else {
+        opacity = 1 - rect.top / visibilityHeight;
+      }
+  
+      section.style.opacity = opacity;
+    });
+  }
+  
+
+function scrollHandler() {
+    animateOnScroll();
+}
+
+function smoothScroll(target) {
+    const element = document.querySelector(target);
+    if (element) {
+    const offset = 50;
+    window.scrollTo({
+        behavior: "smooth",
+        top: element.offsetTop - offset,
+    });
+
+    setTimeout(function () {
+        element.classList.add("active");
+    }, 500);
+    }
+}
+  
+document.querySelectorAll(".menu button").forEach((button) => {
+    button.addEventListener("click", (e) => {
+        e.preventDefault();
+        smoothScroll(button.getAttribute("onclick").split("'")[1]);
+    });
+});
+  
+window.addEventListener("scroll", scrollHandler);
+window.addEventListener("load", scrollHandler);
+  
