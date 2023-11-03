6332c3b269aa061a18dbef71e81439f4cc6878ee
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2023-11-03 12:07:00 +0000

    formatted code - fixed responsive side of website app. fixed img input fixed changed anchors aspect to violetblue

--- src/scripts/global.js
+++ src/scripts/global.js
@@ -5,14 +5,14 @@ function toggleMenu() {
   menu.classList.toggle("active");
   menuToggle.classList.toggle("active");
 
-  if (window.innerWidth > 512) {
+  if (window.innerWidth > 724) {
       menu.classList.remove("active");
       menuToggle.classList.remove("active");
   }
 }   
 
 window.addEventListener("resize", function () {
-  if (window.innerWidth > 512) {
+  if (window.innerWidth > 724) {
     var menu = document.getElementById("menu");
     var menuToggle = document.getElementById("menuToggle");
 
