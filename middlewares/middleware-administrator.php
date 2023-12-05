048392ab0451f6e197e5004e9eb939c2120b79e7
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2023-12-05 18:36:32 +0000

    sem querer dei merge da branch à main ;--;

--- middlewares/middleware-administrator.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-@require_once __DIR__ . '/../validations/session.php';
-
-if (!administrator()) {
-    $home_url = 'http://' . $_SERVER['HTTP_HOST'] . '/php-project/';
-    header('Location: ' . $home_url);
-}
--- middlewares/middleware-not-authenticated.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../validations/session.php';
-
-if (isset($_SESSION['id']) || isset($_COOKIE['id'])) {
-    $home_url = 'http://' . $_SERVER['HTTP_HOST'] . '/php-project/pages/secure';
-    header('Location: ' . $home_url);
-}
