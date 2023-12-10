a80f7c8e8a1c23647d2f6fb9d163088978ded7c9
kromenz <rafael.pires.andre@ipvc.pt>
2023-12-10 21:13:37 +0000

    expenses.php

--- repositories/expense.php
+++ repositories/expense.php
@@ -1,23 +1,6 @@
 <?php
 require_once __DIR__ . '../../db/connection.php';
 
-function getAllExpenses()
-{
-    $query = 'SELECT expenses.*, categories.description AS category_description, methods.description AS payment_description ';
-    $query .= 'FROM expenses ';
-    $query .= 'LEFT JOIN categories ON expenses.category_id = categories.id ';
-    $query .= 'LEFT JOIN methods ON expenses.payment_id = methods.id';
-
-    $PDOStatement = $GLOBALS['pdo']->query($query);
-    $expenses = [];
-    
-    while ($expensesList = $PDOStatement->fetch(PDO::FETCH_ASSOC)) {
-        $expenses[] = $expensesList;
-    }
-
-    return $expenses;
-}
-
 function getAllExpensesById($userId)
 {
     try {
@@ -42,19 +25,6 @@ function getAllExpensesById($userId)
         echo 'Error: ' . $e->getMessage();
         return false;
     }
-}
-
-function getExpensesByPayStatus($isPaid)
-{
-    $query = "SELECT * FROM expenses WHERE paid = :isPaid;";
-    $PDOStatement = $GLOBALS['pdo']->prepare($query);
-    $PDOStatement->bindValue(':isPaid', $isPaid, PDO::PARAM_INT);
-    $PDOStatement->execute();
-    $expenses = [];
-    while ($expense = $PDOStatement->fetch()) {
-        $expenses[] = $expense;
-    }
-    return $expenses;
 }
 
 function getExpensesByCategory($categoryId)
@@ -89,6 +59,22 @@ function getExpensesByPaymentMethod($paymentMethodId)
         $expenses[] = $expense;
     }
     return $expenses;
+}
+
+function getAllCategories()
+{
+    $stmt = $GLOBALS['pdo']->prepare('SELECT * FROM categories WHERE deleted_at IS NULL;');
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function getAllMethods()
+{
+    $stmt = $GLOBALS['pdo']->prepare('SELECT * FROM methods WHERE deleted_at IS NULL;');
+    $stmt->execute();
+
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
 ?>
