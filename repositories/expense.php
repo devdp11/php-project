5fcafa83c6f8fe47381c830c6093fef959baefd1
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2023-12-13 17:23:44 +0000

    query getExpenseById

--- repositories/expense.php
+++ repositories/expense.php
@@ -27,29 +27,22 @@ function getAllExpensesById($userId)
     }
 }
 
-function getExpensesById($expenseId)
+function getExpenseById($expenseId, $pdo)
 {
     try {
         $query = 'SELECT expenses.*, categories.description AS category_description, methods.description AS payment_description ';
         $query .= 'FROM expenses ';
         $query .= 'LEFT JOIN categories ON expenses.category_id = categories.id ';
         $query .= 'LEFT JOIN methods ON expenses.payment_id = methods.id ';
-        $query .= 'WHERE expenses.expense_id = :expenseId AND expenses.deleted_at IS NULL';
+        $query .= 'WHERE expenses.id = :expenseId';
 
-        $PDOStatement = $GLOBALS['pdo']->prepare($query);
+        $PDOStatement = $pdo->prepare($query);
         $PDOStatement->bindParam(':expenseId', $expenseId, PDO::PARAM_INT);
         $PDOStatement->execute();
 
-        $expenses = [];
-
-        while ($expensesList = $PDOStatement->fetch(PDO::FETCH_ASSOC)) {
-            $expenses[] = $expensesList;
-        }
-
-        return $expenses;
+        return $PDOStatement->fetch(PDO::FETCH_ASSOC);
     } catch (PDOException $e) {
-        echo 'Error: ' . $e->getMessage();
-        return false;
+        throw new Exception('Error fetching expense details: ' . $e->getMessage());
     }
 }
 
