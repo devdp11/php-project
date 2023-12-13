dd13a8d10faf3838f3344ac54ca3104fb5a37631
diogoPinheiro11 <pinheirodiogo2003@gmail.com>
2023-12-13 17:23:23 +0000

    valida quando dou update no perfil. perfil ja esta acabado.

--- controllers/user/user.php
+++ controllers/user/user.php
@@ -4,27 +4,39 @@ require_once __DIR__ . '/../../validations/admin/validate-user.php';
 @require_once __DIR__ . '/../../validations/session.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $validatedData = validatedUser($_POST);
+    
+    if (isset($validatedData['invalid'])) {
+        $_SESSION['errors'] = $validatedData['invalid'];
+        
+        header('Location: ../../pages/secure/profile.php');
+        exit();
+    }
 
     $user = [
         'id' => $_SESSION['id'], 
-        'first_name' => $_POST['first_name'],
-        'last_name' => $_POST['last_name'],
-        'email' => $_POST['email'],
-        'country' => $_POST['country'],
-        'birthdate' => $_POST['birthdate'],
-        'updated_at' => $_POST['NOW()'],
+        'first_name' => $validatedData['first_name'],
+        'last_name' => $validatedData['last_name'],
+        'email' => $validatedData['email'],
+        'country' => $validatedData['country'],
+        'birthdate' => $validatedData['birthdate'],
+        'updated_at' => date('Y-m-d H:i:s'),
     ];
 
     $updateSuccess = updateUser($user);
 
     if ($updateSuccess) {
+        $_SESSION['success'] = 'Profile updated successfully.';
+        
         header('Location: ../../pages/secure/profile.php');
         exit();
     } else {
-        echo 'Error updating profile.';
+        $_SESSION['errors'] = ['update' => 'Error updating profile.'];
+        
+        header('Location: ../../pages/secure/profile.php');
+        exit();
     }
 } else {
     echo 'Invalid request method.';
 }
-
 ?>
--- validations/admin/validate-user.php
+++ validations/admin/validate-user.php
@@ -7,31 +7,36 @@ function validatedUser($req)
     }
 
     if (empty($req['first_name']) || strlen($req['first_name']) < 3 || strlen($req['first_name']) > 255) {
-        $errors['first_name'] = 'The First Name field cannot be empty and must be between 3 and 255 characters.';
+        $errors['first_name'] = 'The Name field cannot be empty and must be between 3 and 255 characters';
     }
-    
+
     if (empty($req['last_name']) || strlen($req['last_name']) < 3 || strlen($req['last_name']) > 255) {
-        $errors['last_name'] = 'The Last Name field cannot be empty and must be between 3 and 255 characters.';
+        $errors['last_name'] = 'The Last Name field cannot be empty and must be between 3 and 255 characters';
+    }
+
+    if (!empty($req['email'])) {
+        $user = user();
+        $existingUser = getByEmail($req['email']);
+
+        if ($existingUser && $existingUser['id'] != $user['id']) {
+            $errors['email'] = 'Email already registered in our system.';
+        }
+    } else {
+        $errors['email'] = 'The Email field cannot be empty.';
     }
 
     if (!filter_var($req['email'], FILTER_VALIDATE_EMAIL)) {
-        $errors['email'] = 'The Email field must not be empty and must have an email format, such as: name@example.com.';
+        $errors['email'] = 'The Email field must have a valid email format, for example: name@example.com.';
     }
 
-    if (getByEmail($req['email'])) {
-        $errors['email'] = 'Email already registered in our system.';
-        return ['invalid' => $errors];
+    $currentDate = new DateTime();
+    $birthdate = DateTime::createFromFormat('Y-m-d', $req['birthdate']);
+
+    if ($birthdate > $currentDate) {
+        $errors['birthdate'] = 'Birthdate cannot be a future date.';
     }
 
-    if (empty($req['password']) || !preg_match('/^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^a-zA-Z\d])\S{8,}$/', $req['password'])) {
-        $errors['password'] = 'Password must be at least 8 characters long and contain at least one digit, one uppercase letter, one lowercase letter, and one special character!';
-    }    
-
-    if ($req['confirm_password'] != $req['password']) {
-        $errors['confirm_password'] = 'The Confirm Password field must not be empty and must be the same as the Password field!';
-    }
-
-    $req['administrator'] = !empty($req['administrator']) == 'on' ? true : false;
+    $req['admin'] = !empty($req['admin']) == 'on' ? true : false;
 
     if (isset($errors)) {
         return ['invalid' => $errors];
