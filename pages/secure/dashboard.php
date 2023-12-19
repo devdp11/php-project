0e97af2faaa296c44f9ec249758790787e1207d1
kromenz <rafael.pires.andre@ipvc.pt>
2023-12-19 19:54:24 +0000

    sidebar color, dashboard style

--- pages/secure/dashboard.php
+++ pages/secure/dashboard.php
@@ -17,109 +17,130 @@
 
 <?php include __DIR__ . '/sidebar.php'; ?>
 
-    <div class="p-4 overflow-auto h-100">
-        <nav style="--bs-breadcrumb-divider:'>';font-size:14px">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item"><i class="fa-solid fa-house"></i></li>
-                <li class="breadcrumb-item">Dashboard</li>
-            </ol>
-        </nav>
+<style>
+.style {
+    background-color: white;
+    background: linear-gradient(to right, #8a5bff, #e35bff);
+    border-color: darkviolet;
+    transition: box-shadow 0.3s ease;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+    border-radius: 10px;
+}
 
-        <div class="row row-cols-1 row-cols-md-3 g-2">
-            <div class="col">
-                <div class="card bg-warning">
-                    <div class="card-body">
-                        <h6 class="card-title">Number of your expenses</h6>
-                        <p class="card-text text-muted mt-3">Number of Expenses: <?php echo $expensesCount; ?></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card bg-info text-white">
-                    <div class="card-body">
-                        <h6 class="card-title">Number of your expenses you shared</h6>
-                        <p class="card-text mt-3">Number of Expenses: <?php echo $sharedExpensesCountByMe; ?></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card bg-danger text-white">
-                    <div class="card-body">
-                        <h6 class="card-title">Number of your expenses shared with you</h6>
-                        <p class="card-text mt-3">Number of Expenses: <?php echo $sharedExpensesCountToMe; ?></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card bg-danger text-white">
-                    <div class="card-body">
-                        <h6 class="card-title">Amount of your expenses</h6>
-                        <?php
-                        $displayAmount = isset($sumExpensesAmount) && $sumExpensesAmount !== '' ? $sumExpensesAmount : '0';
-                        ?>
-                        <p class="card-text mt-3">Expense's Amount: <?php echo $displayAmount; ?>€</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card bg-danger text-white">
-                    <div class="card-body">
-                        <h6 class="card-title">Amount of shared expenses</h6>
-                        <p class="card-text mt-3">Expense's Amount: <?php echo $sumSharedExpensesAmount; ?>€</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card bg-success text-white">
-                    <div class="card-body">
-                        <h6 class="card-title">Number of your paid expenses</h6>
-                        <p class="card-text mt-3">Number of Expenses: <?php echo $paidExpensesCount; ?></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card bg-success text-white">
-                    <div class="card-body" <?php if ($futureExpenses > 0) echo 'data-bs-toggle="modal" data-bs-target="#futureDetails" style="cursor:pointer"'; ?>>
-                        <h6 class="card-title">Future Expenses</h6>
-                        <?php if ($futureExpenses > 0): ?>
-                            <p class="my-2">
-                                You have <?php echo $futureExpenses; ?> future expenses. Check them out!
-                            </p>
-                        <?php else: ?>
-                            <p class="my-2">No future expenses</p>
-                        <?php endif; ?>
-                    </div>
+.style:hover {
+    background-color: blueviolet;
+    color: white;
+
+}
+
+.style:hover .card-title {
+    color: white;
+}
+</style>
+
+<div class="p-4 overflow-auto h-100">
+    <nav style="--bs-breadcrumb-divider:'>';font-size:14px">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><i class="fa-solid fa-house"></i></li>
+            <li class="breadcrumb-item">Dashboard</li>
+        </ol>
+    </nav>
+
+    <div class="row row-cols-1 row-cols-md-3 g-2">
+        <div class="col">
+            <div class="card style">
+                <div class="card-body">
+                    <h6 class="card-title">Number of expenses: <?php echo $expensesCount; ?></h6>
                 </div>
             </div>
         </div>
-        
-        <div class="modal fade" id="futureDetails" tabindex="-1" aria-labelledby="futureDetails" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="futureDetails">Future Details</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <?php if (!empty($futureExpensesDetails)): ?>
-                            <?php foreach ($futureExpensesDetails as $expense): ?>
-                                <div class="card mb-3">
-                                    <div class="card-body">
-                                        <h5 class="card-title"><?php echo $expense['description']; ?></h5>
-                                        <p class="card-text">Amount: <?php echo $expense['amount']; ?>€</p>
-                                        <p class="card-text">Category: <?php echo $expense['category_description']; ?></p>
-                                        <p class="card-text">Date: <?php echo $expense['date']; ?></p>
-                                        <p class="card-text">Created At: <?php echo $expense['created_at']; ?></p>
-                                    </div>
-                                </div>
-                            <?php endforeach; ?>
-                        <?php else: ?>
-                            <p>No future expenses</p>
-                        <?php endif; ?>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    </div>
+
+        <div class="col">
+            <div class="card style">
+                <div class="card-body">
+                    <h6 class="card-title">Number of shared expenses: <?php echo $sharedExpensesCountByMe; ?></h6>
                 </div>
             </div>
         </div>
+
+        <div class="col">
+            <div class="card style">
+                <div class="card-body">
+                    <h6 class="card-title">Number of expenses shared with you: <?php echo $sharedExpensesCountToMe; ?>
+                    </h6>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card style">
+                <div class="card-body">
+                    <?php
+            $displayAmount = isset($sumExpensesAmount) && $sumExpensesAmount !== '' ? $sumExpensesAmount : '0';
+            ?>
+                    <h6 class="card-title">Amount spent: <?php echo $displayAmount; ?>€</h6>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card style">
+                <div class="card-body">
+                    <h6 class="card-title">Amount of shared expenses: <?php echo $sumSharedExpensesAmount; ?>€</h6>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card style">
+                <div class="card-body">
+                    <h6 class="card-title">Number of paid expenses: <?php echo $paidExpensesCount; ?></h6>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card style"
+                <?php if ($futureExpenses > 0) echo 'data-bs-toggle="modal" data-bs-target="#futureDetails" style="cursor:pointer"'; ?>>
+                <div class="card-body">
+                    <?php if ($futureExpenses > 0): ?>
+                    <p class="my-2">You have <?php echo $futureExpenses; ?> future expenses. Check them out!</p>
+                    <?php else: ?>
+                    <p class="my-2">No future expenses</p>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="modal fade" id="futureDetails" tabindex="-1" aria-labelledby="futureDetails" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="futureDetails">Future Details</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <?php if (!empty($futureExpensesDetails)): ?>
+                    <?php foreach ($futureExpensesDetails as $expense): ?>
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php echo $expense['description']; ?></h5>
+                            <p class="card-text">Amount: <?php echo $expense['amount']; ?>€</p>
+                            <p class="card-text">Category: <?php echo $expense['category_description']; ?></p>
+                            <p class="card-text">Date: <?php echo $expense['date']; ?></p>
+                            <p class="card-text">Created At: <?php echo $expense['created_at']; ?></p>
+                        </div>
+                    </div>
+                    <?php endforeach; ?>
+                    <?php else: ?>
+                    <p>No future expenses</p>
+                    <?php endif; ?>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
--- pages/secure/sidebar.php
+++ pages/secure/sidebar.php
@@ -6,53 +6,68 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@300;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../resources/styles/global.css">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
 </head>
 
 <style>
-    html,body{
+html,
+body {
     height: 100%;
     font-family: 'Ubuntu', sans-serif;
-  }
-  
-  .mynav{
+}
+
+.mynav {
     color: #fff;
-  }
-  
-  .mynav li a {
+}
+
+.mynav li a {
     color: #fff;
     text-decoration: none;
     width: 100%;
     display: block;
     border-radius: 5px;
     padding: 8px 5px;
-  }
-  
-  .mynav li .btn-side:hover{
-    background: rgba(255,255,255,0.2);
-  }
-  
-  .mynav li a i{
+}
+
+.mynav li .btn-side:hover {
+    background: rgba(255, 255, 255, 0.2);
+}
+
+.mynav li a i {
     width: 25px;
     text-align: center;
-  }
+}
+
+.bg-color {
+    --bs-bg-opacity: 1;
+    background-color: #333 !important;
+}
 </style>
 
 <body>
     <div class="container-fluid p-0 d-flex h-100">
-        <div id="bdSidebar" class="d-flex flex-column flex-shrink-0 p-3 bg-black text-white offcanvas-md offcanvas-start" style="width: 270px;">
+        <div id="bdSidebar"
+            class="d-flex flex-column flex-shrink-0 p-3 bg-color text-white offcanvas-md offcanvas-start"
+            style="width: 270px;">
             <a href="#" class="navbar-brand">
-                <h5 class="h6" style="color:blueviolet"><i class="fa-solid fa-wallet me-2" style="font-size: 18px;"></i>EXPENSE FLOW</h5>
+                <h5 class="h6" style="color:blueviolet"><i class="fa-solid fa-wallet me-2"
+                        style="font-size: 18px;"></i>EXPENSE FLOW</h5>
             </a>
             <hr>
             <ul class="mynav nav nav-pills flex-column mb-auto">
@@ -95,7 +110,8 @@
                     </a>
                 </li>
                 <li class="nav-item py-1 mask <?= $user['admin'] ? '' : 'd-none'; ?>">
-                    <a href="#" class="nav-link text-white btn-side" data-bs-toggle="collapse" data-bs-target="#adminDropdown" aria-expanded="false">
+                    <a href="#" class="nav-link text-white btn-side" data-bs-toggle="collapse"
+                        data-bs-target="#adminDropdown" aria-expanded="false">
                         <i class="fa-solid fa-users" title="Admin"></i>
                         Admin
                     </a>
@@ -119,21 +135,23 @@
             <div class="d-flex align-items-center">
                 <div class="d-flex">
                     <?php if (!empty($user['avatar'])): ?>
-                        <?php
+                    <?php
                             $avatarData = base64_decode($user['avatar']);
                             $avatarSrc = 'data:image/jpeg;base64,' . base64_encode($avatarData);
                         ?>
-                        <img src="<?= $avatarSrc ?>" alt="avatar" class="img-fluid rounded-circle me-2" width="50px">
+                    <img src="<?= $avatarSrc ?>" alt="avatar" class="img-fluid rounded-circle me-2" width="50px">
                     <?php else: ?>
-                        <i class="fas fa-user-circle fa-3x me-2 text-secondary"></i>
+                    <i class="fas fa-user-circle fa-3x me-2 text-secondary"></i>
                     <?php endif;?>
 
                     <span>
-                        <h6 class="mt-2 mb-0"><?= mb_strlen($user['first_name'] ?? '') > 16 ? substr($user['first_name'], 0, 16) . '...' : $user['first_name'] ?? 'Guest' ?></h6>
+                        <h6 class="mt-2 mb-0">
+                            <?= mb_strlen($user['first_name'] ?? '') > 16 ? substr($user['first_name'], 0, 16) . '...' : $user['first_name'] ?? 'Guest' ?>
+                        </h6>
                         <small><?= $user['email'] ?? null ?></small>
-                    </span>   
+                    </span>
                 </div>
-                
+
                 <div class="ms-auto">
                     <form action="../../controllers/auth/signin.php" method="post">
                         <button class="btn btn-outline-danger btn-sm" type="submit" name="user" value="logout">
